9c7328102e006c284bd5690a48ca1e29e4e780b4
tittbit <tittbit@gmail.com>
2025-09-21 02:39:37 +0530

    switched from firebase to bolt

---
+++ prisma/migrations/20250920205201_npx_prisma_generate/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "shop" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "scope" TEXT,
+    "expires" DATETIME,
+    "accessToken" TEXT NOT NULL,
+    "userId" BIGINT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "locale" TEXT,
+    "collaborator" BOOLEAN DEFAULT false,
+    "emailVerified" BOOLEAN DEFAULT false
+);
+
+-- CreateTable
+CREATE TABLE "QRCode" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "productHandle" TEXT NOT NULL,
+    "productVariantId" TEXT NOT NULL,
+    "destination" TEXT NOT NULL,
+    "scans" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
